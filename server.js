d3121a4be94e0138e3c817bf9b017c033acf232f
pâmela santos <pssantos201@gmail.com>
2022-12-14 11:34:54 -0300

    :rocket: add: connections

---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./src/app')
+
+const port = 1313
+
+app.listen(port, () => console.log(`Special Pets on port ${port}`))
---
+++ src/app.js
@@ -0,0 +1,30 @@
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerFile = require("../swagger/swagger_output.json");
+
+require("dotenv-safe").config();
+
+const express = require("express");
+
+const cors = require("cors");
+
+const mongoose = require("./database/dbConnect");
+
+const petsRoutes = require("./routes/petsRoute");
+
+const ownersRoutes = require("./routes/ownersRoute")
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+mongoose.connect();
+
+app.use("/specialpets/pets", petsRoutes);
+app.use("/specialpets/owners", ownersRoutes);
+app.use(
+  "/minha-rota-de-documentacao",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile)
+);
+
+module.exports = app;
