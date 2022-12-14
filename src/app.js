9ddc6791556f2dce356aaa7af0ba294083ea7097
pâmela santos <pssantos201@gmail.com>
2022-12-14 20:12:50 -0300

    :sparkles: feat: app.js

--- src/app.js
+++ src/app.js
@@ -12,17 +12,21 @@ const mongoose = require("./database/dbConnect");
 
 const petsRoutes = require("./routes/petsRoute");
 
-const ownersRoutes = require("./routes/ownersRoute")
+const ownersRoutes = require("./routes/ownersRoute");
+
+const index = require("./routes/index");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 mongoose.connect();
 
+app.use("/", index);
 app.use("/specialpets/pets", petsRoutes);
 app.use("/specialpets/owners", ownersRoutes);
 app.use(
-  "/minha-rota-de-documentacao",
+  "/special-pets-documentation",
   swaggerUi.serve,
   swaggerUi.setup(swaggerFile)
 );
